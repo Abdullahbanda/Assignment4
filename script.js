37f62107c974978ce398b7edfeeedf4c990e6b6d
Abdullahbanda <mhabdullah353@gmail.com>
2026-02-23 14:39:58 +0600

    Array created

---
+++ script.js
@@ -0,0 +1,10 @@
+const jobs = [
+  { id: 1, companyName: "Programming Hero", position: "Frontend Developer", location: "Dhaka", type: "Full-time", salary: "50,000tk", description: "Build scalable UI applications.", status: "all" },
+  { id: 2, companyName: "Fre code camp", position: "Backend Developer", location: "USA", type: "Full-time", salary: "$6000", description: "Develop cloud systems.", status: "all" },
+  { id: 3, companyName: "Scrimba", position: "DevOps Engineer", location: "Canada", type: "Full-time", salary: "$5500", description: "Manage CI/CD pipelines.", status: "all" },
+  { id: 4, companyName: "W3 schools", position: "UI Designer", location: "India", type: "Contract", salary: "$4000", description: "Design user interfaces.", status: "all" },
+  { id: 5, companyName: "Phitron", position: "Assistant Programmer", location: "Remote", type: "Full-time", salary: "75000tk", description: "Analyze and support programming tasks.", status: "all" },
+  { id: 6, companyName: "Prothom kalo", position: "SEO Expert", location: "Dhaka", type: "Full-time", salary: "70000tk", description: "Analayze and Supervise SEO activities.", status: "all" },
+  { id: 7, companyName: "Bd news 24", position: "Mobile Developer", location: "Faridpur", type: "Full-time", salary: "48000tk", description: "Mobile apps development.", status: "all" },
+  { id: 8, companyName: "Mimo", position: "Senior Mobile Developer", location: "Remote", type: "Full-time", salary: "$4200", description: "Ensure product quality.", status: "all" }
+];
